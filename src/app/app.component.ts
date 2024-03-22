e81bb369048f9441eae95b1e6ea7738cde70150a
Gabriel Caetano <gabrielcaetano@outlook.com>
2024-03-22 12:03:10 -0300

    close focus after selectionChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 interface Language {
@@ -26,6 +26,9 @@ interface Language {
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  @ViewChild(MatSelect) select!: MatSelect;
+
   title = 'Gabriel Caetano - Software Engineer';
   languages: Language[] = [
     { value: 'pt', imageUrl: '../assets/portuguese.png' },
@@ -41,5 +44,6 @@ export class AppComponent {
 
   handleSelectionChange() {
     this.translateService.use(this.selectedLanguage.value);
+    this.select.close();
   }
 }
