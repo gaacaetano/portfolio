ce98722a9cbad574b0bf1fefb627c974eee1e645
Gabriel Caetano <gabrielcaetano@outlook.com>
2024-03-26 09:45:07 -0300

    skills and services section

--- src/app/skills-services/skills-services.component.ts
+++ src/app/skills-services/skills-services.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
 interface SkillImage {
@@ -11,14 +12,26 @@ interface SkillImage {
   standalone: true,
   imports: [
     TranslateModule,
+    MatIconModule
   ],
   templateUrl: './skills-services.component.html',
   styleUrl: './skills-services.component.css'
 })
 export class SkillsServicesComponent {
-  images: SkillImage[] = [
-    { skill: 'dotnet', imageUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg' },
-    { skill: 'chsarp', imageUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg' },
-    { skill: 'docker', imageUrl: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg' }
+  skills: SkillImage[] = [
+    { skill: '.NET', imageUrl: '../../assets/skills/dotnetcore.svg' },
+    { skill: 'C#', imageUrl: '../../assets/skills/csharp.svg' },
+    { skill: 'Docker', imageUrl: '../../assets/skills/docker.svg' },
+    { skill: 'Git', imageUrl: '../../assets/skills/git.svg' },
+    { skill: 'MongoDB', imageUrl: '../../assets/skills/mongodb.svg' },
+    { skill: 'MySQL', imageUrl: '../../assets/skills/mysql.svg' },
+    { skill: 'SQL Developer', imageUrl: '../../assets/skills/sqldeveloper.svg' },
+    { skill: 'Angular', imageUrl: '../../assets/skills/angular.svg' },
+    { skill: 'Angular Material', imageUrl: '../../assets/skills/angularmaterial.svg' },
+    { skill: 'Tailwind CSS', imageUrl: '../../assets/skills/tailwind.svg' },
+    { skill: 'Typescript', imageUrl: '../../assets/skills/typescript.svg' },
+    { skill: 'AWS', imageUrl: '../../assets/skills/aws.svg' },
+    { skill: 'Azure', imageUrl: '../../assets/skills/azure.svg' },
+    { skill: 'Firebase', imageUrl: '../../assets/skills/firebase.svg' }
   ];
 }
